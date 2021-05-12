c863267be52bc9cedb7988546bddef1be695af27
pyithuaung <pyithuaung@aceplussolutions.com>
2021-05-12 15:19:32 +0630

    create layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 
 import { Button } from 'reactstrap';
 import { useTranslation } from 'react-i18next';
+import HeaderBar from './components/HeaderBar/HeaderBar';
 
 function App() {
   const { t } = useTranslation();
   return (
-    <div>
-      <h1>Hello new project</h1>
-      <p>{t('TextLabel')}</p>
-      <Button color="secondary" size="lg" disabled>Button</Button>
+    // <div>
+    //   <h1>Hello new project</h1>
+    //   <p>{t('TextLabel')}</p>
+    //   <Button color="secondary" size="lg" disabled>Button</Button>
+    // </div>
+    <div className="App">
+      <HeaderBar></HeaderBar>
+
     </div>
   );
 }
