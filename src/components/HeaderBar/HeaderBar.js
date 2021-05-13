3be701cfe737e01acc62120b522395fe78d5eaf8
pyithuaung <pyithuaung@aceplussolutions.com>
2021-05-13 13:27:58 +0630

    add react-bootstrap and header bar

--- src/components/HeaderBar/HeaderBar.js
+++ src/components/HeaderBar/HeaderBar.js
@@ -1,60 +1,38 @@
 import React, { useState } from 'react';
-import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem,
-    NavbarText,
-    Button
-} from 'reactstrap';
 import cs from './styles.module.css';
+import {
+    Button,
+    Navbar,
+    Nav,
+    NavDropdown
+} from 'react-bootstrap';
 
-const HeaderBar = (props) => {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggle = () => setIsOpen(!isOpen);
+const HeaderBar = () => {
     return (
-        <div>
-            <Navbar className={`${cs.header_bar}`}>
-                <Button color="secondary" className={`${cs.header_bar_button}`}> ペルソナ一覧に戻る</Button>
-                {/* <NavbarToggler onClick={toggle} />
-                <Collapse isOpen={isOpen} navbar>
-                    <Nav className="mr-auto" navbar>
-                        <NavItem>
-                            <NavLink href="/components/">Components</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-                        </NavItem>
-                        <UncontrolledDropdown nav inNavbar>
-                            <DropdownToggle nav caret>
-                                Options
-              </DropdownToggle>
-                            <DropdownMenu right>
-                                <DropdownItem>
-                                    Option 1
-                </DropdownItem>
-                                <DropdownItem>
-                                    Option 2
-                </DropdownItem>
-                                <DropdownItem divider />
-                                <DropdownItem>
-                                    Reset
-                </DropdownItem>
-                            </DropdownMenu>
-                        </UncontrolledDropdown>
-                    </Nav>
-                    <NavbarText>Simple Text</NavbarText>
-                </Collapse> */}
-            </Navbar>
-        </div>
+        <div> <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className={`${cs.header_bar}`}>
+            <Navbar.Brand href="#home" className={`${cs.header_bar_button}`} > Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                <Nav>
+                    <Nav.Link href="#deets">More deets</Nav.Link>
+                    <Nav.Link eventKey={2} href="#memes">
+                        Dank memes
+  </Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar></div>
+
     );
 }
 
