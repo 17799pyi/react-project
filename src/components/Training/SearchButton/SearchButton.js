d9c9aa72d38079575a4f937103328c93b2b5d9f7
Thein Htay <theinhtay@aceplussolution.com>
2021-05-18 16:04:09 +0630

    Add folder for module

--- src/components/Training/SearchButton/SearchButton.js
+++ src/components/Training/SearchButton/SearchButton.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import './SearchButton.css'
+import styles from './styles.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 function SearchButton() {
     return (
         <div>
-            <button className="search-button"><FontAwesomeIcon icon={faSearch} /> Search</button>
+            <button className={`${styles.search_button}`}><FontAwesomeIcon icon={faSearch} /> Search</button>
         </div>
     )
 }
