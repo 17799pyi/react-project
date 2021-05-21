5e168d5e139cb696a10998c1447abb04241ce456
khinmaytun <khinmaytun@aceplussolutions.com>
2021-05-21 10:23:01 +0630

    responsive check 2

--- src/components/Keyword/Keyword.js
+++ src/components/Keyword/Keyword.js
@@ -1,58 +1,35 @@
 import React from 'react'
+import { Container, Row, Col } from 'reactstrap';
+
 import classes from './styles.module.css'
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
+import CloseIcon from '../../assets/images/scenarios/close_icon.png'
+
+import DeleteKeywordButton from '../../components/Button/DeleteKeywordButton'
+import AddKeywordButton from '../../components/Button/AddKeywordButton'
+
 function Keyword() {
     return (
         <div className={classes.keyword_wrapper}>
-            <div className="row mt-3">
-                <div className="col-4">
-                    <input type="text" value="Word 1" className={classes.keyword_input} />
-                </div>
-                <div className="col-8">
-                    <div className="row">
-                        <div className="col-10">
-                            <div className="row">
-                                <div className={classes.keyword_added_box}>
-                                    <span>Sync 1</span>
-                                    <FontAwesomeIcon icon={faTimes} />
-                                </div>
-                                <div className={classes.keyword_added_box}>
-                                    <span>Sync 1</span>
-                                    <FontAwesomeIcon icon={faTimes} />
-                                </div>
-                                <div className={classes.keyword_added_box}>
-                                    <span>Sync 1</span>
-                                    <FontAwesomeIcon icon={faTimes} />
-                                </div>
-                                <div className={classes.keyword_added_box}>
-                                    <span>Sync 1</span>
-                                    <FontAwesomeIcon icon={faTimes} />
-                                </div>
-                                <div className={classes.keyword_added_box}>
-                                    <span>Sync 1</span>
-                                    <FontAwesomeIcon icon={faTimes} />
-                                </div>
-                                <div className={classes.keyword_added_box}>
-                                    <span>Sync 1</span>
-                                    <FontAwesomeIcon icon={faTimes} />
-                                </div>
-                                <div className={classes.keyword_add_btn}>
-                                    <FontAwesomeIcon icon={faPlus} />
-                                    Add Synonyms
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-1">
-                            <div className={classes.remove_keyword}>
-                                <FontAwesomeIcon icon={faTimes} />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Row>
+                <Col lg="4" md="11" sm="11" xs="11" className="order-0 order-lg-0">
+                    <input type="text" value="Word 1" className={`cmn-bg-white-box px-3 py-2 w-100`} />
+                </Col> 
+                <Col lg="7" md="12" className="order-2 order-lg-1 mt-lg-0 mt-3">
+                    <DeleteKeywordButton title="Sync 1" className="px-3 py-2 mr-1 mb-1 bg-transparent"/>
+                    <DeleteKeywordButton title="Sync 1" className="px-3 py-2 mr-1 mb-1 bg-transparent"/>
+                    <DeleteKeywordButton title="Sync 1" className="px-3 py-2 mr-1 mb-1 bg-transparent"/>
+                    <DeleteKeywordButton title="Sync 1" className="px-3 py-2 mr-1 mb-1 bg-transparent"/> 
+                    <AddKeywordButton title="Add Synonyms" className="px-3 py-2 mb-1 bg-transparent"/>
+                </Col>            
+                <Col lg="1" md="1" sm="1" xs="1" className="order-1 order-lg-2 text-center">
+                    <button className="btn no-btn pt-2"><img src={CloseIcon} className="btn-img"/></button>
+                </Col>
+            </Row>
         </div>
     )
 }
