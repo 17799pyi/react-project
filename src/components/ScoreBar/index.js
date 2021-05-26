d0ce6c33655b9c631b257e8919b7d985eca74016
johnsmith58 <aungthiha9885@gmail.com>
2021-05-26 13:33:23 +0630

    add:tran in phase1

--- src/components/ScoreBar/index.js
+++ src/components/ScoreBar/index.js
@@ -3,17 +3,20 @@ import React from 'react';
 import smileImg from '../../assets/images/icons/smile.png'
 import starImg from '../../assets/images/icons/star.png'
 import AcceptButton from '../Button/AcceptButton'
+import { useTranslation } from 'react-i18next';
 
 import classes from './styles.module.css'
 
 const BackgroundBlueLabel = ({ label, className, style}) => {
 
+    const { t } = useTranslation();
+
     return (
        <div className={`${classes.scroll_bar} ${className}`}>
            <div className={classes.scroll_per_sec}>
-                <span>月 .日</span>
+                <span>{t('rateOfRisk.time')}</span>
                 <span>11：00 - 11：12</span>
-                <span>正解率：80%</span>
+                <span>{t('rateOfRisk.correct_answer_rate')}：80%</span>
                 <img src={smileImg} className={classes.w_19}/>
            </div>
            <div className={classes.scroll_btn_sec}>
@@ -22,7 +25,7 @@ const BackgroundBlueLabel = ({ label, className, style}) => {
                     <img src={starImg} className={classes.w_17} />
                     <img src={starImg} className={classes.w_17} />
                </span>
-               <AcceptButton title="受け入れ中"/>
+               <AcceptButton title={t('rateOfRisk.accepting')}/>
            </div>
        </div>
     )
