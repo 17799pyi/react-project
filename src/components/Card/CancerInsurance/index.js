7f07bafa6217721e312d72f86fe7827fddede577
khinmaytun <khinmaytun@aceplussolutions.com>
2021-05-28 16:57:48 +0630

    responsive check 5

--- src/components/Card/CancerInsurance/index.js
+++ src/components/Card/CancerInsurance/index.js
@@ -20,9 +20,9 @@ function CancerInsuranceCard({onEditScenerio}) {
           <p className="mb-0">佐藤　53歳　男性</p>
           <p>スーパーがん保険 本人型</p>
           {/* <p>{t('recruiter.review_of_cancer_insurance')}</p> */}
-          <div className="w-100 d-flex mb-3">
-            <PercentageLabelBox label={t('recruiter.progress_rate')} percentage="33%" className="mr-2"/>            
-            <GeneralButton title={t('recruiter.decision')} onClick={onEditScenerio}/>          
+          <div className="w-100 d-flex flex-wrap mb-2">
+            <PercentageLabelBox label={t('recruiter.progress_rate')} percentage="33%" className="mr-2 mb-2"/>            
+            <GeneralButton title={t('recruiter.decision')} onClick={onEditScenerio} className="mb-2"/>          
           </div>  
         </Col>
       </Row>
