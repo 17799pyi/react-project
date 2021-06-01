f09ab9f4ae83d9e7601441c79b5679472e0ec18e
Thein Htay <theinhtay@aceplussolution.com>
2021-06-02 00:07:12 +0630

    Update CheckHistory

--- src/pages/phase1/HistoryCheck/index.js
+++ src/pages/phase1/HistoryCheck/index.js
@@ -36,19 +36,76 @@ function HistoryCheck() {
       </Row>
       <Row>
         <Col xs="5">
+          {/* <Row> */}
           <p className="font-16 font-weight-bold mb-3">代理店コード(7桁)</p>
           <div className="mb-32 text_box">
-            <input type="text" placeholder="代理店コードを入力してください" className="" />
+            <input type="text" placeholder="代理店コードを入力してください" className="" style={{width: '100%'}}/>
           </div>
           <p className="font-16 mb-3">出先単位で確認したい場合</p>
           <p className="font-16 font-weight-bold mb-3">出先コード(7桁)</p>
-          <div className="mb-32 text_box">
-            <input type="text" placeholder="居場所コードを入力" className="" />
+          <div className="mb-32 text_box" >
+            <input type="text" placeholder="居場所コードを入力" className="" style={{width: '100%'}}/>
           </div>
+          {/* </Row> */}
+          <Row className={`${classes.row_margin}`}>
+            <button className={`${classes.search_btn} mr-3`}>
+              <p>button</p> 
+            </button>
+          </Row>
         </Col>
         <Col xs="7">
           <p className="font-16 font-weight-bold mb-3">募集人一覧</p>
-          
+          <div className="table-responsive mb-4">
+              <table className={`table text-center ${classes.cmn_table}`}>
+                <tr>
+                  <th rowspan="2" className="align-middle" style={{width: '25%'}}>ペルソナ</th>
+                  <th colspan="3" style={{width: '75%'}}>松尾さん</th>
+                  {/* <th style={{width: '23%'}}>岡田さん</th>
+                  <th style={{width: '23%'}}>安田さん</th> */}
+                </tr>
+                <tr>
+                  <th style={{width: '25%'}} className="border-left-0">配偶者ストーリー</th>
+                  <th style={{width: '25%'}}>お子様ストーリー</th>
+                  <th style={{width: '25%'}}>おひとり様ストーリー</th>
+                </tr>
+                <tr>
+                  <td><a href="#" style={{color: 'blue'}}>Afuraku Tarou</a></td>
+                  <td className="bg-green">受講完了</td>
+                  <td className="bg-yellow-dark">受講中</td>
+                  <td className="bg-red">未実装</td>
+                </tr>
+                <tr>
+                  <td><a href="#" style={{color: 'blue'}}>B</a></td>
+                  <td className="bg-green">受講完了</td>
+                  <td className="bg-green">受講完了</td>
+                  <td className="bg-green">受講完了</td>
+                </tr>
+                <tr>
+                  <td><a href="#" style={{color: 'blue'}}>B</a></td>
+                  <td className="bg-green">受講完了</td>
+                  <td className="bg-green">受講完了</td>
+                  <td className="bg-green">受講完了</td>
+                </tr>
+                <tr>
+                  <td><a href="#" style={{color: 'blue'}}>B</a></td>
+                  <td className="bg-green">受講完了</td>
+                  <td className="bg-green">受講完了</td>
+                  <td className="bg-green">受講完了</td>
+                </tr>
+                <tr>
+                  <td><a href="#" style={{color: 'blue'}}>B</a></td>
+                  <td className="bg-green">受講完了</td>
+                  <td className="bg-green">受講完了</td>
+                  <td className="bg-green">受講完了</td>
+                </tr>
+                <tr>
+                  <td><a href="#" style={{color: 'blue'}}>B</a></td>
+                  <td className="bg-green">受講完了</td>
+                  <td className="bg-green">受講完了</td>
+                  <td className="bg-green">受講完了</td>
+                </tr>
+              </table>
+            </div>
         </Col>
       </Row>
     </>
