59f181e5033234ab7481adc1fe46a3454d6ffb19
khinmaytun <khinmaytun@aceplussolutions.com>
2021-06-03 16:34:52 +0630

    auto id

--- src/components/ScoreBar/index.js
+++ src/components/ScoreBar/index.js
@@ -10,27 +10,32 @@ import { useTranslation } from 'react-i18next';
 
 import classes from './styles.module.css'
 
-const BackgroundBlueLabel = ({ label, className, style, percentage}) => {
+const BackgroundBlueLabel = ({ label, className, style, percentage, id}) => {
 
     const { t } = useTranslation();
+    let lastId = 0;
+    const autoId = (prefix = '-scorebar-') => {
+        lastId++;
+        return `${id}${prefix}${lastId}`;
+    }
 
     return (
-       <div className={`${classes.scroll_bar} ${className} ${ (percentage>=70)&& classes.pass}`}>
+       <div className={`${classes.scroll_bar} ${className} ${ (percentage>=70)&& classes.pass}`} id={autoId()}>
            <div className={classes.scroll_per_sec}>
-                <span>2021年2月26日 ({t('rateOfRisk.wednesday')})</span>
-                <span className="ml-2 ml-xl-4">11：00 - 11：12</span>
+                <span id={autoId()}>2021年2月26日 ({t('rateOfRisk.wednesday')})</span>
+                <span className="ml-2 ml-xl-4" id={autoId()}>11：00 - 11：12</span>
            </div>
            <div className={classes.scroll_btn_sec}>
-                <span className="mr-4 pr-0  pr-xl-2">{t('rateOfRisk.correct_answer_rate')}</span>
+                <span className="mr-4 pr-0  pr-xl-2" id={autoId()}>{t('rateOfRisk.correct_answer_rate')}</span>
                <span className={classes.score_star}>
-                    <img src={starImg} className={classes.w_17} />
-                    <img src={starImg} className={classes.w_17} />
-                    <img src={(percentage>=70)?starImgBlack:starImgWhite} className={classes.w_17} />
+                    <img src={starImg} alt="Star Image" className={classes.w_17} id={autoId()}/>
+                    <img src={starImg} alt="Star Image" className={classes.w_17} id={autoId()}/>
+                    <img src={(percentage>=70)?starImgBlack:starImgWhite} alt="Star Image Black" className={classes.w_17} id={autoId()}/>
                </span>
            </div>
            <div className={classes.scroll_btn_sec}>
-                <span className="mr-2 mr-xl-4">({percentage}%)</span>
-                <img src={percentage>=70? smileImg : NoSmileImg} className={classes.w_19}/>
+                <span className="mr-2 mr-xl-4" id={autoId()}>({percentage}%)</span>
+                <img src={percentage>=70? smileImg : NoSmileImg} alt="Smile Image" className={classes.w_19} id={autoId()}/>
            </div>
        </div>
     )
--- src/pages/phase1/RateOfRiskPage/index.js
+++ src/pages/phase1/RateOfRiskPage/index.js
@@ -15,6 +15,12 @@ import classes from './styles.module.css'
 const RateOfRiskPage = () => {
 
     const { t } = useTranslation();
+    let lastId = 0;
+    const autoId = (prefix = 'rate-of-risk-') => {
+        lastId++;
+        return `${prefix}${lastId}`;
+    }
+
     return (
         <>
         <Row className="align-items-start mb-32 pb-2">  
@@ -23,32 +29,32 @@ const RateOfRiskPage = () => {
             </Col>     */}
             <Col>
                 <div className={classes.purpose_sec}>
-                    <span>{t('rateOfRisk.header_1')} <br/><span className="font-weight-normal font-white">{t('rateOfRisk.header_2')}</span></span>
-                    <span>x x x</span>
+                    <span id={autoId()}>{t('rateOfRisk.header_1')} <br/><span className="font-weight-normal font-white">{t('rateOfRisk.header_2')}</span></span>
+                    <span id={autoId()}>x x x</span>
                 </div>
             </Col>                
         </Row>
         <div className="cmn-bg-box p-4">
             <Row className="smallest-padding-box02">
                 <Col xs="3">                    
-                    <BackgroundBlueLabel label={t('rateOfRisk.s_header_1')} className="font-18"/>
+                    <BackgroundBlueLabel label={t('rateOfRisk.s_header_1')} className="font-18" id={autoId()}/>
                 </Col>
                 <Col xs="3">
-                    <BackgroundBlueLabel label={t('rateOfRisk.s_header_2')} className="font-18"/>
+                    <BackgroundBlueLabel label={t('rateOfRisk.s_header_2')} className="font-18" id={autoId()}/>
                 </Col>
                 <Col xs="6">
-                    <BackgroundBlueLabel label={t('rateOfRisk.s_header_3')} className="font-18"/>
+                    <BackgroundBlueLabel label={t('rateOfRisk.s_header_3')} className="font-18" id={autoId()}/>
                 </Col>
             </Row>
             <Row className="smallest-padding-box02 mt-2">
                 <Col xs="3">
-                    <div className={`${classes.top_btn}`}>{t('rateOfRisk.frequency_of_long_term_care')}</div>
+                    <div className={`${classes.top_btn}`} id={autoId()}>{t('rateOfRisk.frequency_of_long_term_care')}</div>
                 </Col>
                 <Col xs="3">
                     <div className={classes.content_outline}>
                         <ul className="point-list">
-                            <li>**</li>
-                            <li>**</li>
+                            <li id={autoId()}>**</li>
+                            <li id={autoId()}>**</li>
                         </ul>
                     </div>
                 </Col>
@@ -57,11 +63,11 @@ const RateOfRiskPage = () => {
                         <Row className="w-100">
                             <Col md="2" className="d-flex align-items-center">
                                 <ul className="point-list">
-                                    <li>**</li>
+                                    <li id={autoId()}>**</li>
                                 </ul>
                             </Col>
                             <Col md="10">
-                                <img src={sample1_img} className=""/>
+                                <img src={sample1_img} alt="Sample Image" className="" id={autoId()}/>
                             </Col>
                         </Row>
                     </div>
@@ -69,13 +75,13 @@ const RateOfRiskPage = () => {
             </Row>
             <Row className="smallest-padding-box02 mt-2">
                 <Col xs="3">
-                    <div className={`${classes.top_btn}`}>{t('rateOfRisk.lifespan_and_healthy_lifespan')}</div>
+                    <div className={`${classes.top_btn}`} id={autoId()}>{t('rateOfRisk.lifespan_and_healthy_lifespan')}</div>
                 </Col>
                 <Col xs="3">
                     <div className={classes.content_outline}>
                         <ul className="point-list">
-                            <li>**</li>
-                            <li>**</li>
+                            <li id={autoId()}>**</li>
+                            <li id={autoId()}>**</li>
                         </ul>
                     </div>
                 </Col>
@@ -84,12 +90,12 @@ const RateOfRiskPage = () => {
                         <Row className="w-100">
                             <Col md="2" className="d-flex align-items-center">
                                 <ul className="point-list">
-                                    <li>**</li>
+                                    <li id={autoId()}>**</li>
                                 </ul>
                             </Col>
                             <Col md="10">
-                                <img src={sample1_img} className="mr-2"/>
-                                <img src={sample1_img} className="mr-2"/>
+                                <img src={sample1_img} alt="Sample Image" className="mr-2" id={autoId()}/>
+                                <img src={sample1_img} alt="Sample Image" className="mr-2" id={autoId()}/>
                             </Col>
                         </Row>
                     </div>
@@ -97,36 +103,36 @@ const RateOfRiskPage = () => {
             </Row>
             <Row className="smallest-padding-box02 mt-2">
                 <Col xs="3">
-                    <div className={`${classes.top_btn}`}>{t('rateOfRisk.causes_of_long_term_care')}</div>
+                    <div className={`${classes.top_btn}`} id={autoId()}>{t('rateOfRisk.causes_of_long_term_care')}</div>
                 </Col>
                 <Col xs="3">
                     <div className={classes.content_outline}>
                         <ul className="point-list">
-                            <li>**</li>
-                            <li>**</li>
+                            <li id={autoId()}>**</li>
+                            <li id={autoId()}>**</li>
                         </ul>
                     </div>
                 </Col>
                 <Col xs="6">
-                    <div className={classes.content_outline}>
+                    <div className={classes.content_outline} id={autoId()}>
                         -
                     </div>
                 </Col>
             </Row>
             <Row className="smallest-padding-box02 mt-2">
                 <Col xs="3">
-                    <div className={`${classes.top_btn} ${classes.content_bottom_extra} ${classes.top_btn_no_triagle}`}>{t('rateOfRisk.long_term_care_period')}</div>
+                    <div className={`${classes.top_btn} ${classes.content_bottom_extra} ${classes.top_btn_no_triagle}`} id={autoId()}>{t('rateOfRisk.long_term_care_period')}</div>
                 </Col>
                 <Col xs="3">
                     <div className={classes.content_outline}>
                      <ul className="point-list">
-                        <li>**</li>
-                        <li>**</li>
+                        <li id={autoId()}>**</li>
+                        <li id={autoId()}>**</li>
                     </ul>
                     </div>
                 </Col>
                 <Col xs="6">
-                    <div className={classes.content_outline}>
+                    <div className={classes.content_outline} id={autoId()}>
                         -
                     </div>
                 </Col>
@@ -134,14 +140,14 @@ const RateOfRiskPage = () => {
             
             <Row className="mt-4 mb-32 pb-4">
                 <Col lg="10" className="mx-auto">
-                    <Link to="/AIScore"><ScoreBar className="mb-3" percentage="75"/></Link>
-                    <Link to="/AIScore"> <ScoreBar percentage="65"/></Link>
+                    <Link to="/AIScore"><ScoreBar className="mb-3" percentage="75" id={autoId()}/></Link>
+                    <Link to="/AIScore"> <ScoreBar percentage="65" id={autoId()}/></Link>
                 </Col>
             </Row>
             
             <Row>
                 <Col className="text-center pt-2">        
-                    <button className={classes.bottom_btn_submit}><Link to="/VideoChat">{t('rateOfRisk.proceed_to_the_next')}</Link></button>
+                    <button className={classes.bottom_btn_submit} id={autoId()}><Link to="/VideoChat">{t('rateOfRisk.proceed_to_the_next')}</Link></button>
                 </Col>
             </Row>
         </div>
